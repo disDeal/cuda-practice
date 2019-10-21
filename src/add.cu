c40aa28ef3a86077611671157ac9683ddd7c7bfc
Alex Komissarov <k.a.komissar@gmail.com>
2019-10-21 14:21:20 +0500

    working Cuda

--- src/add.cu
+++ src/add.cu
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 __global__
-void saxpy(int n, float a, float *x, float *y)
-{
+void saxpy(int n, float a, float *x, float *y) {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   if (i < n) y[i] = a*x[i] + y[i];
 }
 
-int main(void)
-{
+int main(void) {
   int N = 1024*1024;
   float *x, *y, *d_x, *d_y;
   x = (float*)malloc(N*sizeof(float));
--- src/hello.cu
+++ src/hello.cu
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <iostream>
 
-__global__ void cuda_hello(){
-  printf("Hello World from GPU!\n");
+#define N (1024*1024)
+
+__global__ void kernel(float* dA) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  float x = 2.0f * 3.1415926f * (float)idx / (float)N;
+  dA[idx] = sinf(sqrtf(x));
 }
 
-int main() {
-  cuda_hello<<<10, 10>>>(); 
+int main(int argc, char* argv[]) {
+  float* hA; float* dA;
+  hA = (float*) malloc(N * sizeof(float));
+  cudaMalloc((void**) &dA, N*sizeof(float));
+  kernel<<<N/512, 512>>>(dA);
+  cudaMemcpy(hA, dA, N*sizeof(float), cudaMemcpyDeviceToHost);
+  for (int idx = 0; idx < N; idx++) {
+    printf("a[%d] = %0.5f\n", idx, hA[idx]);
+  } 
+  free(hA);
+  cudaFree(dA);
   return 0;
-}
+} 
